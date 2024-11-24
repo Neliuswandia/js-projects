a96956cdf869ac1323e764a4ba3f13d511e193ce
Neliuswandia <nelius.wandia@student.moringaschool.com>
2024-11-24 11:42:04 +0300

    clearing the input box after adding the task

--- todo_list/todo_list.js
+++ todo_list/todo_list.js
@@ -2,11 +2,12 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
 function addTask(){
-    if(inputBox.Value = ''){
-        alert('Please write a task')
+    if(inputBox.value === ''){
+        alert("Please write a task!")
     }else{
         const li = document.createElement("li");
         li.innerText = inputBox.value;
         listContainer.appendChild(li);
     }
+    inputBox.value = '';
 };
